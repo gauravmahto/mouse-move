c96f7f60ad325d8f94a67a28372f8edc815421f9
Gaurav Mahto <gaurav.mahto@aristocrat.com>
2020-09-15 15:59:09 +0530

    Initial commit

---
+++ MouseMove/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MouseMove
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.idleTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Move Move";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(327, 121);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.AcceptsTab = true;
+            this.textBox.Location = new System.Drawing.Point(117, 69);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(327, 38);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "40";
+            // 
+            // idleTextBox
+            // 
+            this.idleTextBox.Location = new System.Drawing.Point(117, 379);
+            this.idleTextBox.Name = "idleTextBox";
+            this.idleTextBox.ReadOnly = true;
+            this.idleTextBox.Size = new System.Drawing.Size(327, 38);
+            this.idleTextBox.TabIndex = 2;
+            this.idleTextBox.TabStop = false;
+            this.idleTextBox.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 493);
+            this.Controls.Add(this.idleTextBox);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TextBox idleTextBox;
+    }
+}
+
---
+++ MouseMove/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MouseMove
+{
+    internal struct LastInputInfo
+    {
+        public uint cbSize;
+        public uint dwTime;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct Input
+    {
+        public uint type;
+        public CombinedInput data;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct CombinedInput
+    {
+        [FieldOffset(0)]
+        public HardwareInput Hardware;
+        [FieldOffset(0)]
+        public KeyboardInput Keyboard;
+        [FieldOffset(0)]
+        public MouseInput Mouse;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct HardwareInput
+    {
+        public uint msg;
+        public ushort paramL;
+        public ushort paramH;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KeyboardInput
+    {
+        public ushort vK;
+        public ushort scan;
+        public uint flags;
+        public uint time;
+        public IntPtr extraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MouseInput
+    {
+        public int x;
+        public int y;
+        public uint mouseData;
+        public uint flags;
+        public uint time;
+        public IntPtr extraInfo;
+    }
+
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LastInputInfo ptrInputInfo);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint numberOfInputs, Input[] inputs, int sizeOfInputStructure);
+
+        static uint GetLastInputTime()
+        {
+            uint idleTime = 0;
+            LastInputInfo lastInputInfo = new LastInputInfo();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            uint envTicks = (uint)Environment.TickCount;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                uint lastInputTick = lastInputInfo.dwTime;
+
+                idleTime = (envTicks - lastInputTick);
+            }
+
+            return ((idleTime > 0) ? (idleTime / 1000) : idleTime);
+        }
+
+        private readonly Random random = new Random();
+        private readonly uint defaultSecs = 40;
+
+        private SynchronizationContext synchronizationContext;
+        private System.Threading.Timer timer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            synchronizationContext = SynchronizationContext.Current;
+        }
+
+        private void ScheduleTask(uint secs, Action task)
+        {
+            if (timer == null)
+            {
+                timer = new System.Threading.Timer(x =>
+                {
+                    task.Invoke();
+                }, null, secs * 1000, secs * 1000);
+            }
+        }
+
+        private void SendMouseInput()
+        {
+            Input input = new Input
+            {
+                type = 0
+            };
+
+            input.data.Mouse = new MouseInput
+            {
+                x = 0,
+                y = 0
+            };
+
+            Input[] inputs = new Input[]
+            {
+                input
+            };
+
+            SendInput(1, inputs, Marshal.SizeOf(typeof(Input)));
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uint secs = defaultSecs;
+            bool start = true;
+
+            if (button1.Text.Equals("Start"))
+            {
+                button1.Text = "Stop";
+                start = true;
+            }
+            else
+            {
+                button1.Text = "Start";
+                start = false;
+            }
+
+            try
+            {
+                string val = textBox.Text.Trim();
+                textBox.Text = val;
+                secs = Convert.ToUInt32(val);
+            }
+            catch
+            {
+                secs = defaultSecs;
+                textBox.Text = defaultSecs.ToString();
+            }
+
+            if (start)
+            {
+                ScheduleTask(secs, (() =>
+                {
+                    synchronizationContext.Post((object o) =>
+                    {
+                        uint idleTime = GetLastInputTime();
+                        idleTextBox.Text = idleTime.ToString();
+
+                        if (idleTime < secs)
+                        {
+                            return;
+                        }
+
+                        this.Cursor = new Cursor(Cursor.Current.Handle);
+
+                        Point point;
+                        int opr = random.Next(100);
+
+                        if (opr < 50)
+                        {
+                            point = new Point(Cursor.Position.X - random.Next(10), Cursor.Position.Y - random.Next(10));
+                        }
+                        else
+                        {
+                            point = new Point(Cursor.Position.X + random.Next(10), Cursor.Position.Y + random.Next(10));
+                        }
+
+                        Cursor.Position = point;
+
+                        try
+                        {
+                            SendMouseInput();
+                        }
+                        catch
+                        {
+                            // noop
+                        }
+
+                    }, sender);
+
+                }));
+            }
+            else
+            {
+                if (timer != null)
+                {
+                    timer.Dispose();
+                    idleTextBox.Text = "0";
+                    timer = null;
+                }
+            }
+        }
+    }
+}
